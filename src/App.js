98386e9a3064ebb8264091e881e40da156a9d04f
Zhanat <sharabidinova2018@gmail.com>
2023-10-28 14:10:55 +0600

    hw6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Weather() {
+    const [cityName, setCityName] = useState('');
+    const [weatherData, setWeatherData] = useState(null);
+
+    const apiKey = '4b8db33390d062f386aa4cab86486d2e\n'; // Поменяйте на свой API ключ OpenWeatherMap
+
+    const searchWeather = () => {
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
+            .then(response => setWeatherData(response.data))
+            .catch(error => console.error('Ошибка:', error));
+    }
+
+    return (
+        <div className="App">
+            <h1 className="title">Отслеживание погоды</h1>
+            <input
+                className="input"
+                type="text"
+                value={cityName}
+                onChange={(e) => setCityName(e.target.value)}
+                placeholder="Введите город"
+            />
+            <button className="button"
+                onClick={searchWeather}>Найти</button>
+            {weatherData && (
+                <div id="weatherData">
+                    <h2>Погода в {weatherData.name}, {weatherData.sys.country}</h2>
+                    <p>Температура: {weatherData.main.temp} °C</p>
+                    <p>Ощущается как: {weatherData.main.feels_like} °C</p>
+                    <p>Влажность: {weatherData.main.humidity} %</p>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default App;
+export default Weather;
